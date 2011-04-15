06decf4d291ae0b0da1164562205b562f8520ad6
Stijn Debrouwere <stijn@stdout.be>
2011-04-15 19:37:01 +0200

    Rough 'n dirty comment management.

--- _director/app.py
+++ _director/app.py
@@ -1,7 +1,8 @@
 import urllib
 import hashlib
-from datetime import datetime
-from flask import Flask, render_template, request
+from datetime import datetime, timedelta
+from flask import Flask, render_template, request, Response, redirect
+from functools import wraps
 from flaskext.sqlalchemy import SQLAlchemy
 import data
 app = Flask(__name__)
@@ -35,6 +36,10 @@ class Comment(db.Model):
                 return True
         
         return False
+    
+    @property    
+    def is_recent(self):
+        return datetime.now() - self.date < timedelta(days=5)
 
     def __init__(self, post, author, email, content, website=None, date=datetime.now(), suikerklontje='', **kwargs):    
         self.post = post
@@ -56,6 +61,28 @@ class Comment(db.Model):
         url += urllib.urlencode({'d': 'identicon', 's': size})
         return url
 
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == data.settings["USERNAME"] and password == data.settings["PASSWORD"]
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 #@app.route("/posts/")
 def posts():
     info = data.get_post_info()
@@ -68,7 +95,16 @@ def posts():
 def comments():
     permalink = request.args.get('permalink')
     matches = Comment.query.filter_by(post=permalink).all()
-    return render_template('comments.html', comments=matches, permalink=permalink)
+    raw_date = "-".join(permalink.split('-')[:3])
+    date = datetime.strptime(raw_date, '%Y-%m-%d')
+    post_is_recent = datetime.now() - date < timedelta(days=10)
+    post_is_lively = len([match for match in matches if match.is_recent])
+    allows_comments = post_is_recent or post_is_lively
+    return render_template('comments.html', 
+        comments=matches, 
+        permalink=permalink, 
+        allows_comments=allows_comments,
+        )
 
 @app.route("/comments/", methods=['POST'])
 def post_comment():
@@ -86,5 +122,22 @@ def post_comment():
     else:
         return '', 400
 
+@app.route("/comments/moderation/", methods=['GET'])
+@requires_auth
+def moderate_comments():
+    comments = Comment.query.order_by(Comment.date.desc())[:20]    
+    return render_template('moderation.html', comments=comments)
+
+@app.route("/comments/<id>/", methods=['POST', 'DELETE'])
+@requires_auth
+def delete_comment(id):
+    if request.form['_method'] != 'DELETE':
+        return '', 400
+    
+    comment = Comment.query.get(id)
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect('/director/comments/moderation/')
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
